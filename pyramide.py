4ec35094a2546fe2f2b1fb1a84fd8261aa55b6b1
OlhaLop <olga_lopatynska@ukr.net>
2018-01-11 15:07:55 +0200

    Add files via upload

---
+++ pyramide.py
@@ -0,0 +1,18 @@
+import cs50
+
+while True:
+    print ("Height: ", end="")
+    height = cs50.get_int()
+    if height>=0 and height<23:
+        break
+
+length=2*height-1
+
+for i in range (height):
+    spaces=length-2*i-1
+    s=int(spaces/2)
+    hashes=length-spaces
+    print(" " * s, end="")
+    print("#" * hashes, end="")
+    print(" " * s, end="")
+    print()
